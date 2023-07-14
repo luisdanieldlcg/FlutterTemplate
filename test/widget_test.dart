a2aa25e9a78aedf625b53b16004e8447a7d04a3f
DanikingRD <danikingrd@gmail.com>
2023-07-14 10:28:38 -0400

    Add theme toggle test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,32 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_firebase_template/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Theme notifier toggles theme', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      const ProviderScope(child: MyApp()),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // find switch widget
+    final switchFinder = find.byType(Switch);
+    expect(switchFinder, findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // if the switch value is false, then the theme mode is light
+    final themeA = tester.widget<Switch>(switchFinder).value;
+
+    // tap the switch
+    await tester.tap(switchFinder);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final themeB = tester.widget<Switch>(switchFinder).value;
+
+    // expect the theme to be toggled, i.e. themeA != themeB
+    expect(themeA != themeB, true);
   });
 }
